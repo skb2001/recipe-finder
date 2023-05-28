41c76d9c2734150a787d2a7bedfc49d1f8e5e475
skb2001 <88270396+skb2001@users.noreply.github.com>
2023-05-28 11:43:22 +0530

    first commit

---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+export const logo = 'https://thumbs.dreamstime.com/b/recipe-word-text-green-leaf-logo-icon-design-white-background-suitable-card-typography-143638205.jpg';
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
